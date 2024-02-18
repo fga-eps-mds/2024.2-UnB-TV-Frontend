e35008629cb90240c1839b2284a69dfe0b34796b
João Victor <joao15victor08@gmail.com>
2024-02-18 20:23:48 -0300

    Altera userAPI

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   videoAPIURL: process.env["videoAPIURL"],
-  usersAPIURL: process.env["usersAPIURL"],
+  // usersAPIURL: process.env["usersAPIURL"],
+  usersAPIURL: "https://unbtv-users.onrender.com/api",
   adminAPIURL: process.env["adminAPIURL"],
   EDUPLAY_CLIENT_KEY: process.env["EDUPLAY_CLIENT_KEY"] as string,
 };
