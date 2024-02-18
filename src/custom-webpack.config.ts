15cf0a07410a5f353e1cb9420b6724e257705695
João Victor <joao15victor08@gmail.com>
2024-02-18 20:21:00 -0300

    Adicina flag para ler variaveies do sistema

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,5 +1,7 @@
 import { EnvironmentPlugin } from 'webpack';
 const Dotenv = require('dotenv-webpack');
 module.exports = {
-    plugins: [new Dotenv()],
+    plugins: [new Dotenv({
+        systemvars: true
+    })],
 }
