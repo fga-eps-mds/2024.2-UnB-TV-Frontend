562ac799c1b66ce2451eb97dabba857979143b18
João Victor <joao15victor08@gmail.com>
2024-03-03 21:00:13 -0300

    Altera videoService endpoint

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  videoAPIURL: process.env["videoAPIURL"],
+  // videoAPIURL: process.env["videoAPIURL"],
+  videoAPIURL: "https://unbtv-videoservice.onrender.com/api",
   // usersAPIURL: process.env["usersAPIURL"],
   usersAPIURL: "https://unbtv-users.onrender.com/api",
   adminAPIURL: process.env["adminAPIURL"],
