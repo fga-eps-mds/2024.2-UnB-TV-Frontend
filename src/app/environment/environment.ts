0e6197fafc5aa57a7d5a484f29717414718fe667
João Victor <joao15victor08@gmail.com>
2024-03-05 11:42:07 -0300

    Altera favicon e icone central

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   videoAPIURL: "https://unbtv-videoservice.onrender.com/api",
   // usersAPIURL: process.env["usersAPIURL"],
   usersAPIURL: "https://unbtv-users.onrender.com/api",
-  adminAPIURL: process.env["adminAPIURL"],
+  // adminAPIURL: process.env["adminAPIURL"],
+  adminAPIURL: "https://unbtv-admin.onrender.com/api",
   EDUPLAY_CLIENT_KEY: process.env["EDUPLAY_CLIENT_KEY"] as string,
 };
