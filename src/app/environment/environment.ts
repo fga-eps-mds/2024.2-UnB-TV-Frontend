a24f73a83c88aea144f591217d544e95abd5c5db
João Victor <joao15victor08@gmail.com>
2024-05-13 13:32:59 -0300

    Atualiza endpoint users

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   // videoAPIURL: process.env["videoAPIURL"],
   videoAPIURL: "https://unbtv-videoservice.onrender.com/api",
   // usersAPIURL: process.env["usersAPIURL"],
-  usersAPIURL: "https://unbtv-users.onrender.com/api",
+  usersAPIURL: "https://unbtv.vercel.app/api",
   // adminAPIURL: process.env["adminAPIURL"],
   adminAPIURL: "https://unbtv-admin.onrender.com/api",
   EDUPLAY_CLIENT_KEY: process.env["EDUPLAY_CLIENT_KEY"] as string,
