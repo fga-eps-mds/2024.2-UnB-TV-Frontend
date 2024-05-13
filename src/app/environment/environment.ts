3d286039fecbedcb584d5e0d0a814dd609365d92
João Victor <joao15victor08@gmail.com>
2024-05-13 14:49:18 -0300

    Atualiza endpoint adminservice

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   // usersAPIURL: process.env["usersAPIURL"],
   usersAPIURL: "https://unbtv.vercel.app/api",
   // adminAPIURL: process.env["adminAPIURL"],
-  adminAPIURL: "https://unbtv-admin.onrender.com/api",
+  adminAPIURL: "https://unbtv-adminservice.vercel.app",
   EDUPLAY_CLIENT_KEY: process.env["EDUPLAY_CLIENT_KEY"] as string,
 };
