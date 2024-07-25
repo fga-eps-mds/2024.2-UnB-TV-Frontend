909580f3760ac85d957cfded578a6a8957245474
Jauzimm <joaovitorso071@gmail.com>
2024-07-25 10:26:12 -0300

    Adicao HTML e inicio da estrutura da tabela categoria

--- src/app/pages/category-table/category-table.component.ts
+++ src/app/pages/category-table/category-table.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UNB_TV_CHANNEL_ID } from 'src/app/app.constant';
+import { VideoService } from 'src/app/services/video.service';
+import { Catalog } from 'src/shared/model/catalog.model';
+import { IVideo } from 'src/shared/model/video.model';
 
 @Component({
   selector: 'app-category-table',
-  standalone: true,
-  imports: [],
   templateUrl: './category-table.component.html',
   styleUrl: './category-table.component.css'
 })
 export class CategoryTableComponent {
+  unbTvChannelId = UNB_TV_CHANNEL_ID;
+  videosEduplay: IVideo[] = [];
+  unbTvVideos: IVideo[] = [];
+  aggregatedVideos: any[] = [];
+  catalog: Catalog = new Catalog();
+
+  constructor(
+    private videoService: VideoService,
+    private router: Router
+  ) {};
+  
+  ngOnInit(): void{
+    this.findAll();
+  }
+  
+  findAll(): void {
+    this.videoService.findAll().subscribe({
+      next: (data) => {
+        this.videosEduplay = data.body?.videoList ?? [];
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        this.filterVideosByChannel(this.videosEduplay);
+        this.videosCatalog(this.unbTvVideos);
+        this.aggregateVideosByCategory();
+      }
+    })
+  }
 
 }
+
