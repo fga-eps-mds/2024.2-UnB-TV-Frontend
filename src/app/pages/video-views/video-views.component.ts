789404cc84058b4f7f48e023d1b5ed3a8da6cbd0
Benjamim Lacerda <benjamim.lacerda16@gmail.com>
2024-07-25 11:12:35 -0300

    Adicionar as funcoes de filtro e busca

--- src/app/pages/video-views/video-views.component.ts
+++ src/app/pages/video-views/video-views.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UNB_TV_CHANNEL_ID } from 'src/app/app.constant';
+import { VideoService } from 'src/app/services/video.service';
+import { Catalog } from 'src/shared/model/catalog.model';
+import { IVideo } from 'src/shared/model/video.model';
 
 @Component({
   selector: 'app-video-views',
@@ -8,5 +12,40 @@ import { Component } from '@angular/core';
   styleUrl: './video-views.component.css'
 })
 export class VideoViewsComponent {
-  
+  unbTvChannelId = UNB_TV_CHANNEL_ID;
+  videosEduplay: IVideo[] = [];
+  unbTvVideos: IVideo[] = [];
+  catalog: Catalog = new Catalog();
+
+  constructor(private videoService: VideoService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.videoService.findAll().subscribe({
+      next: (data) => {
+        this.videosEduplay = data.body?.videoList ?? [];
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        this.filterVideosByChannel(this.videosEduplay);
+        this.videoService.videosCatalog(this.unbTvVideos); // Chamando a função do serviço
+        //this.cleanDescriptions();
+      },
+    });
+  } 
+
+  filterVideosByChannel(videos: IVideo[]): void {
+    videos.forEach((video) => {
+      const channel = video?.channels;
+
+      if (channel)
+        if (channel[0].id === this.unbTvChannelId) this.unbTvVideos.push(video);
+    });
+  }
+
 }
