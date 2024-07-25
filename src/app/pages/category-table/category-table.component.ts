33cf33049c34bce054f94fc2694d7e1fbe73217a
Jauzimm <joaovitorso071@gmail.com>
2024-07-25 10:19:28 -0300

    Criacao do componente categoria e adicao das rotas

---
+++ src/app/pages/category-table/category-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './category-table.component.html',
+  styleUrl: './category-table.component.css'
+})
+export class CategoryTableComponent {
+
+}
