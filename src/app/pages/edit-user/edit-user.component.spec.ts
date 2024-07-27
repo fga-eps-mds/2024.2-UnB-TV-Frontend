adac478be33be45f03a79423a43d2f527d6c6af3
victorleaoo <victor.pessoal1203@gmail.com>
2024-07-27 10:59:34 -0300

    arrumando testes que estavam quebrando anteriormente (registercomponent e editusercomponent)

--- src/app/pages/edit-user/edit-user.component.spec.ts
+++ src/app/pages/edit-user/edit-user.component.spec.ts
@@ -6,110 +6,103 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { of, throwError } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
 import { ProfileComponent } from '../profile/profile.component';
-import { MessageService } from 'primeng/api';
-import { DropdownModule } from 'primeng/dropdown';
 import { AlertService } from 'src/app/services/alert.service';
+import { DropdownModule } from 'primeng/dropdown';
+import { IError } from 'src/shared/model/http-error.model'
 
 const mockData: any = {
-  "name": "Mario",
-  "email": "mario@gmail.com",
-  "connection": "ALUNO",
-}
+  name: "Mario",
+  email: "mario@gmail.com",
+  connection: "ALUNO",
+};
+
+const mockError: IError = { detail: 'An error occurred' };
 
 class UserServiceMock {
-  constructor() { }
   updateUser() {
     return of({ success: true });
   }
   getUser() {
-    return of({ success: true });
+    return of(mockData);
   }
   getVinculo() {
-    return of({ success: true });
+    return of(['ALUNO', 'PROFESSOR']);
   }
 }
 
 class AlertServiceMock {
-  errorMessage() {
-  }
-  showMessage() {
-  }
+  errorMessage() {}
+  showMessage() {}
 }
 
-// describe('EditUserComponent', () => {
-//   let component: EditUserComponent;
-//   let fixture: ComponentFixture<EditUserComponent>;
-//   let userService: UserService;
-//   let alertService: AlertService
+describe('EditUserComponent', () => {
+  let component: EditUserComponent;
+  let fixture: ComponentFixture<EditUserComponent>;
+  let userService: UserService;
+  let alertService: AlertService;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule, RouterTestingModule.withRoutes(
-//         [
-//           { path: 'profile', component: ProfileComponent },
-//         ]
-//       ), ReactiveFormsModule, DropdownModule],
-//       declarations: [EditUserComponent],
-//       providers: [{ provide: UserService, useValue: new UserServiceMock() }, { provide: AlertService, useValue: new AlertServiceMock() }, FormBuilder, MessageService],
-//     })
-//       .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'profile', component: ProfileComponent },
+        ]),
+        ReactiveFormsModule,
+        DropdownModule
+      ],
+      declarations: [EditUserComponent],
+      providers: [
+        { provide: UserService, useValue: new UserServiceMock() },
+        { provide: AlertService, useValue: new AlertServiceMock() },
+        FormBuilder,
+      ],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(EditUserComponent);
-//     component = fixture.componentInstance;
-//     userService = TestBed.inject(UserService);
-//     alertService = TestBed.inject(AlertService);
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(EditUserComponent);
+    component = fixture.componentInstance;
+    userService = TestBed.inject(UserService);
+    alertService = TestBed.inject(AlertService);
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should call getUser and return an error', () => {
-//     fixture.detectChanges();
-//     const mySpy = spyOn(userService, 'getUser').and.returnValue(throwError(() => new Error('Erro')));
-//     component.getUser();
-//     expect(mySpy).toHaveBeenCalled();
-//   });
+  it('should call getUser and handle success response', () => {
+    spyOn(userService, 'getUser').and.returnValue(of(mockData));
+    spyOn(component, 'initializeForm').and.callThrough();
+    component.getUser();
+    expect(component.userData).toEqual(mockData);
+    expect(component.initializeForm).toHaveBeenCalled();
+  });
+  
+  it('should call getVinculo and handle success response', () => {
+    spyOn(userService, 'getVinculo').and.returnValue(of(['ALUNO', 'PROFESSOR']));
+    component.getVinculo();
+    expect(component.vinculo).toEqual([
+      { name: 'ALUNO' },
+      { name: 'PROFESSOR' }
+    ]);
+  });
 
-//   it('should call getVinculo and return an error', () => {
-//     fixture.detectChanges();
-//     const mySpy = spyOn(userService, 'getVinculo').and.returnValue(throwError(() => new Error('Erro')));
-//     component.getVinculo();
-//     expect(mySpy).toHaveBeenCalled();
-//   });
+  it('should call updateUser method when the form is valid and submitted', () => {
+    spyOn(component, 'updateUser').and.callThrough();
+    spyOn(userService, 'updateUser').and.returnValue(of({ success: true }));
+    spyOn(alertService, 'showMessage');
+    const form = component.userForm;
+    form.setValue(mockData);
+    component.userId = 1;
 
-//   it('should call updateUser method when the form is submitted', () => {
-//     fixture.detectChanges();
-//     spyOn(component, 'updateUser').and.callThrough();
-//     const form = component.userForm;
-//     form.setValue(mockData);
-//     component.userId = mockData.id;
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    submitButton.click();
 
-//     const submitButton = fixture.nativeElement.querySelector(
-//       'button[type="submit"]'
-//     );
-//     submitButton.click();
-
-//     expect(component.updateUser).toHaveBeenCalled();
-//   });
-
-//   it('should call showMessage when form is not valid', () => {
-//     fixture.detectChanges();
-//     const mySpy = spyOn(alertService, 'showMessage');
-//     component.updateUser();
-//     expect(mySpy).toHaveBeenCalledWith('info',
-//       'Alerta',
-//       'Preencha todos os campos corretamente!');
-//   });
-
-//   it('should call updateUser and return an error', () => {
-//     fixture.detectChanges();
-//     const form = component.userForm;
-//     form.setValue(mockData);
-//     const mySpy = spyOn(userService, 'updateUser').and.returnValue(throwError(() => new Error('Erro')));
-//     component.updateUser();
-//     expect(mySpy).toHaveBeenCalled();
-//   });
-
-// });
+    expect(component.updateUser).toHaveBeenCalled();
+    expect(alertService.showMessage).toHaveBeenCalledWith(
+      'success',
+      'Sucesso',
+      'Usuário atualizado com sucesso!'
+    );
+  });
+});
--- src/app/pages/register/register.component.spec.ts
+++ src/app/pages/register/register.component.spec.ts
@@ -11,37 +11,34 @@ import { UserService } from 'src/app/services/user.service';
 import { DropdownModule } from 'primeng/dropdown';
 
 const mockData: any = {
-  "name": "Mario",
-  "email": "mario@gmail.com",
-  "connection": "ALUNO",
-  "password": "123456",
-  "confirmPassword": "123456",
-}
+  name: "Mario",
+  email: "mario@gmail.com",
+  connection: { name: "ALUNO" },
+  password: "123456",
+  confirmPassword: "123456",
+};
 
 const mockDataError: any = {
-  "name": "Mario",
-  "email": "",
-  "connection": "ALUNO",
-  "password": "123456",
-  "confirmPassword": "123456",
-}
+  name: "Mario",
+  email: "",
+  connection: { name: "ALUNO" },
+  password: "123456",
+  confirmPassword: "123456",
+};
 
 class AuthServiceMock {
-  constructor() { }
   registerUser() {
     return of({ success: true });
   }
 }
 
 class UserServiceMock {
-  constructor() { }
   getVinculo() {
-    return of({ success: true });
+    return of(['ALUNO', 'PROFESSOR']);
   }
 }
 
 class AlertServiceMock {
-  constructor() { }
   showMessage() {
     return of({ success: true });
   }
@@ -50,89 +47,90 @@ class AlertServiceMock {
   }
 }
 
-// describe('RegisterComponent', () => {
-//   let component: RegisterComponent;
-//   let fixture: ComponentFixture<RegisterComponent>;
-//   let authService: AuthService;
-//   let alertService: AlertService;
-//   let userService: UserService;
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let alertService: AlertService;
+  let userService: UserService;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [HttpClientTestingModule, ReactiveFormsModule, DropdownModule,
-//         RouterTestingModule.withRoutes(
-//           [
-//             { path: 'activateAccount', component: ActiveAccountComponent },
-//           ]
-//         )
-//       ],
-//       providers: [
-//         { provide: UserService, useValue: new UserServiceMock() },
-//         { provide: AuthService, useValue: new AuthServiceMock() },
-//         { provide: AlertService, useValue: new AlertServiceMock() },
-//         FormBuilder,
-//       ],
-//       declarations: [RegisterComponent]
-//     })
-//       .compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        DropdownModule,
+        RouterTestingModule.withRoutes([{ path: 'activateAccount', component: ActiveAccountComponent }])
+      ],
+      providers: [
+        { provide: UserService, useValue: new UserServiceMock() },
+        { provide: AuthService, useValue: new AuthServiceMock() },
+        { provide: AlertService, useValue: new AlertServiceMock() },
+        FormBuilder,
+      ],
+      declarations: [RegisterComponent],
+    }).compileComponents();
 
-//     fixture = TestBed.createComponent(RegisterComponent);
-//     component = fixture.componentInstance;
-//     authService = TestBed.inject(AuthService);
-//     alertService = TestBed.inject(AlertService);
-//     userService = TestBed.inject(UserService);
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    alertService = TestBed.inject(AlertService);
+    userService = TestBed.inject(UserService);
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should have a valid form on initialization', () => {
-//     fixture.detectChanges();
-//     expect(component.userForm).toBeTruthy();
-//   });
+  it('should have a valid form on initialization', () => {
+    fixture.detectChanges();
+    expect(component.userForm).toBeTruthy();
+  });
 
-//   it('should call register method when the form is submitted', () => {
-//     spyOn(component, 'register').and.callThrough();
-//     const alertSpy = spyOn(alertService, 'showMessage').and.callThrough();
-//     fixture.detectChanges();
-//     const form = component.userForm;
-//     form.setValue(mockData);
+  it('should call register method when the form is submitted', () => {
+    spyOn(component, 'register').and.callThrough();
+    const alertSpy = spyOn(alertService, 'showMessage').and.callThrough();
+    fixture.detectChanges();
+    const form = component.userForm;
+    form.setValue(mockData);
 
-//     const submitButton = fixture.nativeElement.querySelector(
-//       'button[type="submit"]'
-//     );
-//     submitButton.click();
+    const submitButton = fixture.nativeElement.querySelector('button[type="submit"]');
+    submitButton.click();
 
-//     expect(component.register).toHaveBeenCalled();
-//     expect(alertSpy).toHaveBeenCalled();
-//   });
+    expect(component.register).toHaveBeenCalled();
+    expect(alertSpy).toHaveBeenCalled();
+  });
 
-//   it('should call register and return message when form is invalid', () => {
-//     fixture.detectChanges();
-//     const form = component.userForm;
-//     form.setValue(mockDataError);
-//     const alertSpy = spyOn(alertService, 'showMessage').and.callThrough();
-//     const mySpy = spyOn(authService, 'registerUser').and.returnValue(of({ success: true }));
-//     component.register();
-//     expect(alertSpy).toHaveBeenCalled();
-//   });
+  it('should call register and return message when form is invalid', () => {
+    fixture.detectChanges();
+    const form = component.userForm;
+    form.setValue(mockDataError);
+    const alertSpy = spyOn(alertService, 'showMessage').and.callThrough();
+    component.register();
+    expect(alertSpy).toHaveBeenCalled();
+  });
 
-//   it('should call register and return an error', () => {
-//     fixture.detectChanges();
-//     const form = component.userForm;
-//     form.setValue(mockData);
-//     const mySpy = spyOn(authService, 'registerUser').and.returnValue(throwError(() => new Error('Erro')));
-//     component.register();
-//     expect(mySpy).toHaveBeenCalled();
-//   });
+  it('should call register and return an error', () => {
+    fixture.detectChanges();
+    const form = component.userForm;
+    form.setValue(mockData);
+    const mySpy = spyOn(authService, 'registerUser').and.returnValue(throwError(() => new Error('Erro')));
+    component.register();
+    expect(mySpy).toHaveBeenCalled();
+  });
 
-//   it('should call getVinculo and return an error', () => {
-//     fixture.detectChanges();
-//     const mySpy = spyOn(userService, 'getVinculo').and.returnValue(throwError(() => new Error('Erro')));
-//     component.getVinculo();
-//     expect(mySpy).toHaveBeenCalled();
-//   });
+  it('should call getVinculo and return an error', () => {
+    fixture.detectChanges();
+    const mySpy = spyOn(userService, 'getVinculo').and.returnValue(throwError(() => new Error('Erro')));
+    component.getVinculo();
+    expect(mySpy).toHaveBeenCalled();
+  });
 
-// });
+  it('should get vinculo from userService', () => {
+    const mockVinculo = ['ALUNO', 'PROFESSOR'];
+    spyOn(userService, 'getVinculo').and.returnValue(of(mockVinculo));
+    component.getVinculo();
+    expect(component.vinculo).toEqual([{ name: 'ALUNO' }, { name: 'PROFESSOR' }]);
+  });
+});
