948d5019383992f77695b01259c5bbc8abe819ed
Vinicius Assumpção de Araujo <78980842+viniman27@users.noreply.github.com>
2024-07-28 16:10:10 -0300

    Correcao home-admin.component.spec.ts

--- src/app/pages/home-admin/home-admin.component.spec.ts
+++ src/app/pages/home-admin/home-admin.component.spec.ts
@@ -1,21 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeAdminComponent } from './home-admin.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { VideoService } from 'src/app/services/video.service';
+import { ConfirmationService } from 'primeng/api';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HomeAdminComponent', () => {
   let component: HomeAdminComponent;
   let fixture: ComponentFixture<HomeAdminComponent>;
+  let authService: AuthService;
+  let confirmationService: ConfirmationService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeAdminComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeAdminComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [AuthService, VideoService, ConfirmationService]
+    }).compileComponents();
     fixture = TestBed.createComponent(HomeAdminComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    authService = TestBed.inject(AuthService);
+    confirmationService = TestBed.inject(ConfirmationService);
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+/*  it('should call logoutUser and confirm logout', () => {
+    spyOn(confirmationService, 'confirm').and.callFake((confirm) => confirm.accept());
+    spyOn(authService, 'logout').and.callThrough();
+
+    component.logoutUser();
+
+    expect(confirmationService.confirm).toHaveBeenCalled();
+    expect(authService.logout).toHaveBeenCalled();
+  }); */
+}); 
