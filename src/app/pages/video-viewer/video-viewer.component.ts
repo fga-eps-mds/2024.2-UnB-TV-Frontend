70aac6dab8707f8410bde96e37c349755fe9d9bd
gabrielhrlima <ghrl2003@gmail.com>
2024-08-02 11:46:54 -0300

    Adiciona fucionalidade e html da interface

--- src/app/pages/video-viewer/video-viewer.component.ts
+++ src/app/pages/video-viewer/video-viewer.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpResponse } from '@angular/common/http';
 import { VideoService } from '../../services/video.service';
 import { IVideo, Video } from 'src/shared/model/video.model';
+import { UserService } from 'src/app/services/user.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-video-viewer',
@@ -15,7 +17,10 @@ export class VideoViewerComponent implements OnInit {
   showDescription = false;
   video: IVideo = new Video();
   idVideo!: number;
+  isWatchLater = true;
   eduplayVideoUrl = "https://eduplay.rnp.br/portal/video/embed/";
+  userId: string = '';
+  user :any;
 
   expandDescription() {
     this.showDescription = !this.showDescription;
@@ -24,13 +29,38 @@ export class VideoViewerComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private videoService: VideoService,
+    private userService: UserService
   ) { }
 
   ngOnInit(): void {
     const iframe = document.getElementById('embeddedVideo') as HTMLIFrameElement;
     this.idVideo = this.route.snapshot.params['idVideo'];
+    console.log('ID do vídeo:', this.idVideo);
+
+    this.setUserIdFromToken(localStorage.getItem('token') as string);
+    this.getUserDetails();
     this.findVideoById();
     iframe.src = this.eduplayVideoUrl + this.idVideo;
+  }
+
+  setUserIdFromToken(token: string) {
+    const decodedToken: any = jwt_decode(token);
+    this.userId = decodedToken.id;
+    console.log('Decoded User ID:', this.userId);
+  }
+
+  getUserDetails() {
+    this.userService.getUser(this.userId).subscribe({
+      next: (user) => {
+        console.log('Fetched User:', user); // Log para verificar o objeto `user`
+        this.user = user;
+        this.checkWatchLaterStatus();
+        console.log(this.isWatchLater);
+      },
+      error: (err) => {
+        console.error('Error fetching user details', err);
+      }
+    });
   }
 
   findVideoById = () => {
@@ -41,8 +71,51 @@ export class VideoViewerComponent implements OnInit {
           ? this.video.description
           : '';
       },
+      error: (err) => {
+        console.error('Error fetching video details', err);
+      }
     });
   };
+
+  toggleWatchLater() {
+    console.log('User ID when toggling watch later:', this.userId);
+    this.isWatchLater = !this.isWatchLater;
+    if (this.isWatchLater) {
+      this.videoService.addToWatchLater(this.idVideo.toString(), this.userId.toString()).subscribe({
+        next: () => {
+          console.log('Video added to watch later list');
+          alert('Vídeo adicionado à lista de "Assistir Mais Tarde".');
+        },
+        error: (err) => {
+          console.error('Error adding to watch later', err);
+        }
+      });
+    } else {
+      this.videoService.removeFromWatchLater(this.idVideo.toString(), this.userId.toString()).subscribe({
+        next: () => {
+          console.log('Video removed from watch later list');
+          alert('Vídeo removido da lista de "Assistir Mais Tarde".');
+        },
+        error: (err) => {
+          console.error('Error removing from watch later', err);
+        }
+      });
+    }
+  }
+
+  checkWatchLaterStatus() {
+    console.log(`Checking watch later status for video ID: ${this.idVideo}, User ID: ${this.userId}`);
+    this.videoService.checkWatchLater(this.idVideo.toString(), this.userId.toString()).subscribe({
+      next: (response) => {
+        console.log(response); // Exibe a resposta completa
+        this.isWatchLater = response.status; // Acessa a propriedade status do objeto response
+        console.log('Watch later status:', this.isWatchLater);
+      },
+      error: (err) => {
+        console.error('Error checking watch later status', err);
+      }
+    });
+  }
 
   getVideoUrl(): string {
     return `${this.eduplayVideoUrl}${this.idVideo}`;
