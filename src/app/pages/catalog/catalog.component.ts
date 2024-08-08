df43dc1108dc3b86d391fbf4bf56f4ff44b9e417
Ruan-Carvalho <ruansobreira11@gmail.com>
2024-08-07 22:24:33 -0300

    chamar atributos e fazer as verificações

--- src/app/pages/catalog/catalog.component.ts
+++ src/app/pages/catalog/catalog.component.ts
@@ -5,7 +5,9 @@ import { VideoService } from 'src/app/services/video.service';
 import { Catalog } from 'src/shared/model/catalog.model';
 import { IVideo } from 'src/shared/model/video.model';
 import { FormsModule } from '@angular/forms';
-
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-catalog',
@@ -19,49 +21,87 @@ export class CatalogComponent {
   catalog: Catalog = new Catalog();
   filteredVideos: IVideo[] = [];
   filterTitle: string = '';
+  filterFavorite: boolean = false;
+  favoriteVideos: IVideo[] = [];
+  userId: string = ''; // Adicionado para armazenar o ID do usuário
+  user: any; // Adicionado para armazenar os detalhes do usuário
+  isAuthenticated: boolean = false; // Propriedade pública para o estado de autenticação
 
-  constructor(private videoService: VideoService, private router: Router) {}
+  constructor(
+    private videoService: VideoService, 
+    private router: Router
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
 
   ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+   if (this.isAuthenticated) {
+     this.setUserIdFromToken(localStorage.getItem('token') as string);
+     this.getUserDetails();
+   }
     this.findAll();
-    this.filteredVideos = this.unbTvVideos;
+  }
+
+  setUserIdFromToken(token: string) {
+    const decodedToken: any = jwt_decode(token);
+    this.userId = decodedToken.id;
+  }
+
+  getUserDetails() {
+    this.userService.getUser(this.userId).subscribe({
+      next: (user) => {
+        this.user = user;
+        this.getFavoriteVideos(); // Carregue os vídeos de "assistir mais tarde" após obter os detalhes do usuário
+      },
+      error: (err) => {
+        console.error('Error fetching user details', err);
+      }
+    });
   }
 
   findAll(): void {
     this.videoService.findAll().subscribe({
       next: (data) => {
         this.videosEduplay = data.body?.videoList ?? [];
+        this.filterVideosByChannel(this.videosEduplay);
+        this.filterVideos(); // Atualize a filtragem após carregar todos os vídeos
+        this.videoService.videosCatalog(this.unbTvVideos, this.catalog);
       },
       error: (error) => {
         console.log(error);
-      },
-      complete: () => {
-        this.filterVideosByChannel(this.videosEduplay);
-        this.videoService.videosCatalog(this.unbTvVideos, this.catalog);
-      },
+      }
     });
   }
 
-  filterVideosByChannel(videos: IVideo[]): void {
-    videos.forEach((video) => {
-      const channel = video?.channels;
-
-      if (channel)
-        if (channel[0].id === this.unbTvChannelId) this.unbTvVideos.push(video);
-    });
+  getFavoriteVideos(): void {
+    if (this.isAuthenticated) {
+      this.videoService.getFavoriteVideos(this.userId).subscribe({
+        next: (data) => {
+          console.log('Resposta completa da API para vídeos de "favoritos":', data);
+           // Verifique se `videoList` existe e é um array
+          if (data && Array.isArray(data.videoList)) {
+            console.log('videoList existe e é um array:', data.videoList);
+            const favorite_videos_ids = data.videoList.map((item: any) => String(item.video_id)); // Converta IDs para string
+            console.log('IDs dos vídeos "favoritos":', favorite_videos_ids);  
+            this.favoriteVideos = this.unbTvVideos.filter(video => favorite_videos_ids.includes(String(video.id))); // Converta IDs para string
+            console.log('Vídeos marcados como "favoritos" após filtragem:', this.favoriteVideos);
+          } else {
+            console.warn('A estrutura da resposta da API não está conforme o esperado:', data);
+          }
+           this.filterVideos(); // Atualize a filtragem após carregar os vídeos de "favoritos"
+        },
+        error: (error) => {
+          console.log('Erro ao buscar vídeos marcados como "favoritos"', error);
+        }
+      });
+    }
   }
 
-  filterVideos() {
-    this.filteredVideos = this.unbTvVideos.filter(video => 
-      (this.filterTitle ? video.title?.toLowerCase().includes(this.filterTitle.toLowerCase()) : true) ||
-      (this.filterTitle ? video.description?.toLowerCase().includes(this.filterTitle.toLowerCase()) : true) ||
-      (this.filterTitle ? video.keywords?.toLowerCase().includes(this.filterTitle.toLowerCase()) : true) ||
-      (this.filterTitle ? video.catalog?.toLowerCase().includes(this.filterTitle.toLowerCase()) : true)
-    );
+  onFilterFavoriteVideosChange() {
+    if (this.filterFavorite) {
+      this.getFavoriteVideos();
+    } else {
+      this.filterVideos();
+    }
   }
-
-  onProgramClick(videos: IVideo[]) {
-    this.videoService.setVideosCatalog(videos);
-    this.router.navigate(['/videos']);
-  }
-}
