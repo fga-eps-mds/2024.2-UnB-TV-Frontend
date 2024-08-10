a583be8d947a4301c0a466ba730d78160bce0970
gabrielhrlima <ghrl2003@gmail.com>
2024-08-09 23:56:41 -0300

    Adiciona o teste da funcao filterVideosByRecord

--- src/app/pages/record/record.component.spec.ts
+++ src/app/pages/record/record.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RecordComponent } from './record.component';
+import { VideoService } from 'src/app/services/video.service';
 
 describe('RecordComponent', () => {
   let component: RecordComponent;
@@ -8,9 +9,10 @@ describe('RecordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RecordComponent]
-    })
-    .compileComponents();
+      imports: [HttpClientTestingModule],
+      declarations: [RecordComponent],
+      providers: [VideoService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RecordComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,33 @@ describe('RecordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should filter videos by record and set filteredVideos correctly', () => {
+    const recordVideos = {
+      videos: {
+        190329: true,
+        190330: true,
+      },
+    };
+
+    const unbTvVideos = [
+      { id: 190329, title: 'Video Title 1' },
+      { id: 190330, title: 'Video Title 2' },
+      { id: 190331, title: 'Video Title 3' },
+    ];
+
+    component.recordVideos = recordVideos;
+    component.unbTvVideos = unbTvVideos;
+
+    component.filterVideosByRecord();
+    fixture.detectChanges();
+
+    const expectedFilteredVideos = [
+      { id: 190329, title: 'Video Title 1' },
+      { id: 190330, title: 'Video Title 2' },
+    ];
+
+    expect(component.filteredVideos).toEqual(expectedFilteredVideos);
   });
 });
