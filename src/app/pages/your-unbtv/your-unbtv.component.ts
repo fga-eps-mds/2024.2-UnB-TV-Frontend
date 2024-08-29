af7361397dcc141874bf48c40187a034dbdfb363
Benjamim Lacerda <benjamim.lacerda16@gmail.com>
2024-08-29 10:20:04 -0300

    Adiciona component ao your-unbtv

---
+++ src/app/pages/your-unbtv/your-unbtv.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-your-unbtv',
+  templateUrl: './your-unbtv.component.html',
+  styleUrls: ['./your-unbtv.component.css']
+})
+export class YourUnBTVComponent {
+
+  constructor(
+  ) {};
+
+  ngOnInit(): void {
+    
+  }
+
+}
