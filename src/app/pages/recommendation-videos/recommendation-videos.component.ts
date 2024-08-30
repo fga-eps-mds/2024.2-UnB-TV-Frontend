e25c9334d97d3d1628f8756884151a7a67693b29
Jauzimm <joaovitorso071@gmail.com>
2024-08-30 19:13:13 -0300

    Adicao da funcao para obter detalhes do usuario

--- src/app/pages/recommendation-videos/recommendation-videos.component.ts
+++ src/app/pages/recommendation-videos/recommendation-videos.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UNB_TV_CHANNEL_ID } from 'src/app/app.constant';
 import { Catalog } from 'src/shared/model/catalog.model';
 import { IVideo } from 'src/shared/model/video.model';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
+import { UserService } from '../../services/user.service';
+import { VideoService } from '../../services/video.service';
 
 @Component({
   selector: 'app-recommendation-videos',
@@ -31,5 +35,22 @@ export class RecommendationVideosComponent implements OnInit {
     } catch (error) {
       console.error('Erro ao buscar os vídeos:', error);
     }
+  }
+
+  setUserIdFromToken(token: string) {
+    const decodedToken: any = jwt_decode(token);
+    this.userId = decodedToken.id;
+  }
+
+  getUserDetails() {
+    this.userService.getUser(this.userId).subscribe({
+      next: (user) => {
+        this.user = user;
+        this.recommendVideosByRecord();
+      },
+      error: (err) => {
+        console.error('Error fetching user details', err);
+      }
+    });
   }
 }
