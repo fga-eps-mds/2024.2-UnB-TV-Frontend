bdb62c68c94c6f2bde96227841da9010a7629fba
gabrielhrlima <ghrl2003@gmail.com>
2024-08-30 08:56:30 -0300

    Adiciona e testa a funcao que permite a troca dos cargos

--- src/app/pages/update-role/update-role.component.ts
+++ src/app/pages/update-role/update-role.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import jwt_decode from 'jwt-decode';
 
-
 @Component({
   selector: 'app-update-role',
   templateUrl: './update-role.component.html',
@@ -13,9 +12,9 @@ export class UpdateRoleComponent implements OnInit {
   users: any = [];
   userId: number = 0;
 
-  filterInputValue: string = "";
-  filterConnectionValue: string = ""
-  
+  filterInputValue: string = '';
+  filterConnectionValue: string = '';
+
   total: number = 0;
   pageSize: number = 10;
   pageIndex: number = 0;
@@ -25,10 +24,10 @@ export class UpdateRoleComponent implements OnInit {
   ngOnInit(): void {
     this.getUserid();
     this.getAllUsers();
-  };
+  }
 
-  updateUserRole(id: number) {
-    this.userService.updateUserRole(id).subscribe({
+  updateUserRole(id: number, role: string) {
+    this.userService.updateUserRoleSuperAdmin(id, role).subscribe({
       next: (data) => {
         console.log(data);
       },
@@ -36,44 +35,52 @@ export class UpdateRoleComponent implements OnInit {
         console.error('Erro', erro);
       },
     });
-  };
+  }
 
   getUserid() {
     const decodedToken: any = jwt_decode(
       localStorage.getItem('token') as string
     );
     this.userId = decodedToken.id;
-  };
+  }
 
   filterUser() {
     this.pageIndex = 0;
     this.getAllUsers();
-  };
+  }
 
   onPaginateChange(event: PageEvent) {
     this.pageSize = event.pageSize;
     this.pageIndex = event.pageIndex;
 
     this.getAllUsers();
-  };
+  }
 
   getAllUsers() {
-    this.userService.getAllUsers({ 
-      nameEmail: this.filterInputValue,
-      connection: this.filterConnectionValue, 
-      limit: this.pageSize, 
-      offset: (this.pageIndex * this.pageSize) 
-    }).subscribe({
-      next: (data: Response) => {
-        this.users = data.body;
-        if (data && data.headers && data.headers.has("x-total-count")) {
-          const totalCountHeader = data.headers.get("x-total-count");
-          this.total = Number.parseInt(totalCountHeader ?? '0', 10); 
-        }
-      },
-      error: (erro) => {
-        console.error('Erro', erro);
-      },
-    });
+    this.userService
+      .getAllUsers({
+        nameEmail: this.filterInputValue,
+        connection: this.filterConnectionValue,
+        limit: this.pageSize,
+        offset: this.pageIndex * this.pageSize,
+      })
+      .subscribe({
+        next: (data: Response) => {
+          this.users = data.body;
+          if (data && data.headers && data.headers.has('x-total-count')) {
+            const totalCountHeader = data.headers.get('x-total-count');
+            this.total = Number.parseInt(totalCountHeader ?? '0', 10);
+          }
+        },
+        error: (erro) => {
+          console.error('Erro', erro);
+        },
+      });
+  }
+  onRoleChange(userId: number, event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const newRole = selectElement.value;
+
+    this.updateUserRole(userId, newRole);
   }
 }
