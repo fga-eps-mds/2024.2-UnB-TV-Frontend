9119db9a5a8e1fd0f4fca03e4f22e050b9c36f05
Jauzimm <joaovitorso071@gmail.com>
2024-08-30 19:01:13 -0300

    Adicao do construtor, variaveis e funcao oninit

--- src/app/pages/recommendation-videos/recommendation-videos.component.ts
+++ src/app/pages/recommendation-videos/recommendation-videos.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { UNB_TV_CHANNEL_ID } from 'src/app/app.constant';
+import { Catalog } from 'src/shared/model/catalog.model';
+import { IVideo } from 'src/shared/model/video.model';
 
 @Component({
   selector: 'app-recommendation-videos',
-  standalone: true,
-  imports: [],
   templateUrl: './recommendation-videos.component.html',
-  styleUrl: './recommendation-videos.component.css'
+  styleUrls: ['./recommendation-videos.component.css']
 })
-export class RecommendationVideosComponent {
+export class RecommendationVideosComponent implements OnInit {
+  unbTvChannelId = UNB_TV_CHANNEL_ID;
+  videosEduplay: IVideo[] = [];
+  unbTvVideos: IVideo[] = [];
+  catalog: Catalog = new Catalog();
+  userId: string;
+  user: any;
+  recommendVideos: IVideo[] = [];
+  
+  constructor(
+    private userService: UserService,
+    private videoService: VideoService,
+    private router: Router
+  ) {}
 
+  async ngOnInit(): Promise<void> {
+    this.setUserIdFromToken(localStorage.getItem('token') as string);
+    try {
+      await this.findAll();
+      this.getUserDetails();
+    } catch (error) {
+      console.error('Erro ao buscar os vídeos:', error);
+    }
+  }
 }
